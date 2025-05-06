4981997956fb2cf21f9c55669ce42054e5809f19
matiuho <matiasvconchaf@gmail.com>
2025-05-06 11:08:45 -0400

    oiiuni j j

--- GreenEnergy/src/main/java/com/example/GreenEnergy/GreenEnergyApplication.java
+++ GreenEnergy/src/main/java/com/example/GreenEnergy/GreenEnergyApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GreenEnergyApplication {
 
+
+
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GreenEnergyApplication.class, args);
