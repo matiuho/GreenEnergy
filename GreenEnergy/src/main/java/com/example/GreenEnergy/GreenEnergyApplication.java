d4a3d56df2911b547a8de1c77d0d5e11aa57c692
matiuho <142462529+matiuho@users.noreply.github.com>
2025-05-06 11:04:37 -0400

    Principal
    todo aqui

---
+++ GreenEnergy/src/main/java/com/example/GreenEnergy/GreenEnergyApplication.java
@@ -0,0 +1,13 @@
+package com.example.GreenEnergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenEnergyApplication.class, args);
+	}
+
+}
---
+++ GreenEnergy/src/test/java/com/example/GreenEnergy/GreenEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreenEnergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenEnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
