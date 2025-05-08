2aa3b7e73030989b8ad6bac4885648cece51a06f
matiuho <matiasvconchaf@gmail.com>
2025-05-08 17:14:59 -0400

    Gestion de usuario
    ahora si creado bien

--- GestionDeUsuarios/src/main/java/com/example/GestionDeUsuarios/GestionDeUsuariosApplication.java
+++ GestionDeUsuarios/src/main/java/com/example/GestionDeUsuarios/GestionDeUsuariosApplication.java
@@ -1,4 +1,4 @@
-package com.example.Gestion.de.Usuarios;
+package com.example.GestionDeUsuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- GestionDeUsuarios/src/test/java/com/example/GestionDeUsuarios/GestionDeUsuariosApplicationTests.java
+++ GestionDeUsuarios/src/test/java/com/example/GestionDeUsuarios/GestionDeUsuariosApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Gestion.de.Usuarios;
+package com.example.GestionDeUsuarios;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
