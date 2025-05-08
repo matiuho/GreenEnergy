7eaf62f2d53f2051867cd1d99b616e8d54011bd5
matiuho <matiasvconchaf@gmail.com>
2025-05-07 22:00:18 -0400

    error que sale
    ayudaaa samuel

--- Autenticación/src/main/java/com/example/Autenticacion/model/Usuario.java
+++ Autenticación/src/main/java/com/example/Autenticacion/model/Usuario.java
@@ -1,14 +1,29 @@
 package com.example.Autenticacion.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "usuario")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class Usuario {
+    @Id //llave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false, length = 30)
     private String password;
+
 }
--- Autenticación/src/main/java/com/example/Autenticacion/repository/UsuarioRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.Autenticacion.repository;
-
-public interface UsuarioRepository {
-
-}
