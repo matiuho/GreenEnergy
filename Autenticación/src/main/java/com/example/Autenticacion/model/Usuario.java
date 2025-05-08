ea30d48634d87acb269d4bce48fe307a6b5b0aa5
samuel__ignacio <sa.villanueva@duocuc.cl>
2025-05-07 21:23:22 -0400

    agregando;)}

--- Autenticación/src/main/java/com/example/Autenticacion/model/Usuario.java
+++ Autenticación/src/main/java/com/example/Autenticacion/model/Usuario.java
@@ -1,7 +1,14 @@
 package com.example.Autenticacion.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Usuario {
     private String username;
 
 }
-hola matias
