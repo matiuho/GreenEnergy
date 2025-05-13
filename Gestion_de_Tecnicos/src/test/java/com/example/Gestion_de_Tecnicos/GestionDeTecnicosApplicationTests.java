0920444a5149dce48b780e9fd9b2e2646266e0f6
samuel__ignacio <sa.villanueva@duocuc.cl>
2025-05-12 21:56:35 -0400

    Add files via upload

---
+++ Gestion_de_Tecnicos/src/test/java/com/example/Gestion_de_Tecnicos/GestionDeTecnicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_de_Tecnicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeTecnicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
