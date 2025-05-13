45fc073b1205c1cf2bd4605b789f94654f38f5b3
samuel__ignacio <sa.villanueva@duocuc.cl>
2025-05-12 21:52:29 -0400

    dsfsdf
    sfsdf

--- Notificaciones/src/main/java/com/example/Notificaciones/model/NotificacionModel.java
+++ Notificaciones/src/main/java/com/example/Notificaciones/model/NotificacionModel.java
@@ -1,6 +1,5 @@
 package com.example.Notificaciones.model;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
