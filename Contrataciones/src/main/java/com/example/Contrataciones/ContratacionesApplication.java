486ccb53fd510fe8e5eb5d3b2ec0866ab39b5134
matiuho <matiasvconchaf@gmail.com>
2025-05-13 16:23:30 -0400

    agregado contrataciones
    microservicio nuevo

---
+++ Contrataciones/src/main/java/com/example/Contrataciones/ContratacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Contrataciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratacionesApplication.class, args);
+	}
+
+}
