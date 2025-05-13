46fe3b0e0b4833e2b8c7a26b27249e12cf2833c4
samuel__ignacio <sa.villanueva@duocuc.cl>
2025-05-12 23:34:42 -0400

    Add files via upload

---
+++ Gestion_de_Mantenimiento/src/main/java/com/example/Gestion_de_Mantenimiento/GestionDeMantenimientoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_de_Mantenimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeMantenimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeMantenimientoApplication.class, args);
+	}
+
+}
---
+++ Gestion_de_Mantenimiento/src/test/java/com/example/Gestion_de_Mantenimiento/GestionDeMantenimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_de_Mantenimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeMantenimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
