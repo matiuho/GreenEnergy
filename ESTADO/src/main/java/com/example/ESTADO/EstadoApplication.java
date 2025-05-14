dce2a4211470922c8b6929456c276bff8a0ff7b9
samuel__ignacio <sa.villanueva@duocuc.cl>
2025-05-13 23:11:41 -0400

    Add files via upload

---
+++ ESTADO/src/main/java/com/example/ESTADO/EstadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ESTADO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstadoApplication.class, args);
+	}
+
+}
