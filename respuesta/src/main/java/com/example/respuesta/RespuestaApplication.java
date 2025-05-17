6415c09a0d8cf48e6f461e31e87f62c27b485eef
samuel__ignacio <sa.villanueva@duocuc.cl>
2025-05-17 15:15:30 -0400

    Add files via upload

---
+++ respuesta/src/main/java/com/example/respuesta/RespuestaApplication.java
@@ -0,0 +1,13 @@
+package com.example.respuesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespuestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespuestaApplication.class, args);
+	}
+
+}
---
+++ respuesta/src/test/java/com/example/respuesta/RespuestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.respuesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespuestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
