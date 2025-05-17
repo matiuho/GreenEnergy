12c12fe5ced0360aa4a064a9b2f8088de3551f78
samuel__ignacio <sa.villanueva@duocuc.cl>
2025-05-17 15:01:33 -0400

    Add files via upload

---
+++ dirección/src/main/java/com/example/direccion/DireccionApplication.java
@@ -0,0 +1,13 @@
+package com.example.direccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DireccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DireccionApplication.class, args);
+	}
+
+}
---
+++ dirección/src/test/java/com/example/direccion/DireccionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.direccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DireccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
