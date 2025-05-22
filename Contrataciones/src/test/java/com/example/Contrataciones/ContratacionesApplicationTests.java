0372c9ea163558db8ab40d1ca1c0fd1a10bf1819
matiuho <matiasvconchaf@gmail.com>
2025-05-22 01:01:22 -0400

    contrataciones

---
+++ Contrataciones/src/test/java/com/example/Contrataciones/ContratacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Contrataciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
