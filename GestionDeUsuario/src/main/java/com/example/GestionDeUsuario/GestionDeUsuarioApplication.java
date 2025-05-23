694e7853cd28d12c8ca9e7793b8dbae02b2cc836
matiuho <matiasvconchaf@gmail.com>
2025-05-23 14:29:11 -0400

    Usuario

---
+++ GestionDeUsuario/src/main/java/com/example/GestionDeUsuario/GestionDeUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionDeUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeUsuarioApplication.class, args);
+	}
+
+}
---
+++ GestionDeUsuario/src/test/java/com/example/GestionDeUsuario/GestionDeUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionDeUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
