e108d314f936d757d673582c77edb765043a451f
matiuho <matiasvconchaf@gmail.com>
2025-06-13 23:25:25 -0400

    cambios requeridos

--- start-services.bat
+++ start-services.bat
@@ -1,10 +1,22 @@
 @echo off
-echo Iniciando microservicios...
+echo ==============================
+echo INICIANDO MICROSERVICIOS...
+echo ==============================
 
+start cmd /k "cd categoria && mvn spring-boot:run"
+start cmd /k "cd Contrataciones && mvn spring-boot:run"
+start cmd /k "cd dirección && mvn spring-boot:run"
 start cmd /k "cd Estado && mvn spring-boot:run"
 start cmd /k "cd GestionDeUsuario && mvn spring-boot:run"
-start cmd /k "cd Contrataciones && mvn spring-boot:run"
+start cmd /k "cd GestionUsuario && mvn spring-boot:run"
+start cmd /k "cd Proyecto && mvn spring-boot:run"
+start cmd /k "cd resena && mvn spring-boot:run"
+start cmd /k "cd respuesta && mvn spring-boot:run"
 start cmd /k "cd Roles && mvn spring-boot:run"
+start cmd /k "cd servicio && mvn spring-boot:run"
+start cmd /k "cd Soporte && mvn spring-boot:run"
 
-echo Todos los microservicios han sido iniciados en ventanas separadas.
+echo ==============================
+echo Todos los microservicios fueron iniciados en ventanas separadas.
+echo ==============================
 pause
