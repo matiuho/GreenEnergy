602fc629e0f6a169079c90bcc1be156c8a20a8aa
samuel__ignacio <sa.villanueva@duocuc.cl>
2025-06-18 17:23:38 -0400

    arreglar

--- dirección/src/test/java/com/example/direccion/Controller/DireccionControllerTest.java
+++ dirección/src/test/java/com/example/direccion/Controller/DireccionControllerTest.java
@@ -1,7 +1,12 @@
 package com.example.direccion.Controller;
 
+import java.util.Arrays;
 import java.util.List;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,18 +15,29 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.example.direccion.controller.DireccionController;
 import com.example.direccion.model.Direccion;
 import com.example.direccion.service.DireccionService;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//cargar el controlador que se va a simular
 @WebMvcTest(DireccionController.class)
 public class DireccionControllerTest {
+
+
+   
     @MockBean
     private DireccionService direccionService;
+
+    //crear un mock proporcionado por spring
     @Autowired
     private MockMvc mockMvc;
-    //crear una reserva ficticia para la respuesta del servicio
+
+
+
+    @Test
+    void getAllDirecciones_returnsOkAndJson() {
         List<Direccion> listaDireccion = Arrays.asList(new Direccion(1L,"Avenida el Guanaco",1,null));
 
         //identificar el comportamiento del servicio
-        when(direccionService
+        when(direccionService.getDirecciones()).thenReturn(listaDireccion);
 
 
         //Ejecutar la funcion del controlador
@@ -29,7 +45,9 @@ public class DireccionControllerTest {
         //verficar que la respuesta sea 200 OK
         //validar que el archivo json contenga los id
         try{
-            mockMvc.perform(get("api/reservas")).andExpect(status().isOk()).andExpect(jsonPath("$[0].id").value(1L));
+            mockMvc.perform(get("api/reservas"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].id").value(1L));
         }catch(Exception e){
 
         }
--- dirección/src/test/java/com/example/direccion/Controller/RegionControllerTest.java
+++ dirección/src/test/java/com/example/direccion/Controller/RegionControllerTest.java
@@ -1,5 +1,53 @@
 package com.example.direccion.Controller;
 
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.direccion.model.Region;
+import com.example.direccion.service.RegionService;
+
+@WebMvcTest(RegionControllerTest.class)
 public class RegionControllerTest {
+
+  
+    @MockBean
+    private RegionService regionService;
+
+     //crear un mock proporcionado por spring
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getAllRegion_returnsOKAndJson(){
+        //crear una reserva ficticia para la respuesta del servicio
+        List<Region> listaRegion = Arrays.asList(new Region(1L,"Region Prueba"));
+
+        //identificar el comportamiento del servicio
+        when(regionService.()).thenReturn(listaRegion);
+
+        //Ejecutar la funcion del controlador
+        //Ejecutar el metodo GET (endpoint)
+        //verficar que la respuesta sea 200 OK
+        //validar que el archivo json contenga los id
+        try{
+            mockMvc.perform(get("api/reservas")).andExpect(status().isOk()).andExpect(jsonPath("$[0].id").value(1L));
+        }catch(Exception e){
+
+        }
+        
+
+    }
+
+
+
+
 
 }
