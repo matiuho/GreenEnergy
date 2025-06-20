f1bbaed5381f0abf35d5f7a892bf8e353d5e2883
matiuho <142462529+matiuho@users.noreply.github.com>
2025-06-19 21:55:04 -0400

    resena micro nuevo

---
+++ Resena/src/main/java/com/example/Resena/ResenaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Resena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenaApplication.class, args);
+	}
+
+}
---
+++ Resena/src/test/java/com/example/Resena/ResenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Resena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
